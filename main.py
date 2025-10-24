b9f8e267db69209081bf5b375caa649a652c87a1
ana.medina <ana.medina@segurosbolivar.com>
2025-10-24 15:36:37 -0500

    Agrega soporte para full_export en process_collection

--- main.py
+++ main.py
@@ -187,107 +187,107 @@ def get_last_execution_time_from_bq(collection_name: str, database_name: str) ->
 @functions_framework.http
 def export_firestore_to_bigquery(request):
     print("✅ Request entró a la función")
-    start_time = datetime.now(timezone.utc)
+    #start_time = datetime.now(timezone.utc)
     request_json = request.get_json(silent=True)
     print(f'🔹 Payload recibido: {request_json}')
 
-    if not request_json or 'collection' not in request_json or 'table' not in request_json:
-        return ({'error': 'Missing collection or table parameter in request'}), 400
+    # if not request_json or 'collection' not in request_json or 'table' not in request_json:
+    #     return ({'error': 'Missing collection or table parameter in request'}), 400
 
-    var_main_collection = request_json['collection']
-    var_table_id = request_json['table']
-    handle_subcollections = request_json.get('handle_subcollections', False)
-    var_database = request_json.get('database', '(default)')
-    updated_field = request_json.get('updated_field')  # Nombre del campo en Firestore
-    full_export = request_json.get('full_export', False)
-    page_size = request_json.get('page_size', 500)
-    print(f'🟢 Parámetros -> Collection: {var_main_collection}, Table: {var_table_id}, Subcollections: {handle_subcollections}, DB: {var_database}')
+    # var_main_collection = request_json['collection']
+    # var_table_id = request_json['table']
+    # handle_subcollections = request_json.get('handle_subcollections', False)
+    # var_database = request_json.get('database', '(default)')
+    # updated_field = request_json.get('updated_field')  # Nombre del campo en Firestore
+    # full_export = request_json.get('full_export', False)
+    # page_size = request_json.get('page_size', 500)
+    # print(f'🟢 Parámetros -> Collection: {var_main_collection}, Table: {var_table_id}, Subcollections: {handle_subcollections}, DB: {var_database}')
 
    
-    updated_after = None
-    if not full_export and updated_field:
-        updated_after = get_last_execution_time_from_bq(var_main_collection, var_database)
+    # updated_after = None
+    # if not full_export and updated_field:
+    #     updated_after = get_last_execution_time_from_bq(var_main_collection, var_database)
 
-    # Cargar secretos y crear clientes
-    service_account_info = json.loads(access_secret_version("sb-operacional-zone", "sb-xops-prod_appspot_gserviceaccount"))
-    firestore_client = firestore.Client(credentials=service_account.Credentials.from_service_account_info(service_account_info), project='sb-xops-prod', database=var_database)
-    bigquery_client = bigquery.Client(project='sb-operacional-zone')
-    var_dataset_id = 'firestore'
+    # # Cargar secretos y crear clientes
+    # service_account_info = json.loads(access_secret_version("sb-operacional-zone", "sb-xops-prod_appspot_gserviceaccount"))
+    # firestore_client = firestore.Client(credentials=service_account.Credentials.from_service_account_info(service_account_info), project='sb-xops-prod', database=var_database)
+    # bigquery_client = bigquery.Client(project='sb-operacional-zone')
+    # var_dataset_id = 'firestore'
 
-    # Procesar colección
-    print(f"🔍 Procesando colección: {var_main_collection}")
-    example_docs, fields = process_collection(firestore_client, var_main_collection, page_size=page_size, handle_subcollections=handle_subcollections,updated_after=updated_after,updated_field=updated_field)
-    if not example_docs:
-        return ({'error': 'No documents found in the Firestore collection'}), 404
+    # # Procesar colección
+    # print(f"🔍 Procesando colección: {var_main_collection}")
+    # example_docs, fields = process_collection(firestore_client, var_main_collection, page_size=page_size, handle_subcollections=handle_subcollections,updated_after=updated_after,updated_field=updated_field)
+    # if not example_docs:
+    #     return ({'error': 'No documents found in the Firestore collection'}), 404
 
-    print(f"✅ Documentos extraídos: {len(example_docs)}")
+    # print(f"✅ Documentos extraídos: {len(example_docs)}")
 
-    temp_file_path = '/tmp/firestore_data.json'
-    print('📝 Creando archivo JSON temporal...')
-    with open(temp_file_path, 'w', encoding='utf-8') as temp_file:
-        for doc in example_docs:
-            temp_file.write(json.dumps({k: serialize_value(v) for k, v in doc.items()}, ensure_ascii=False) + '\n')
-    print('📝 Archivo JSON temporal creado:', temp_file_path)
+    # temp_file_path = '/tmp/firestore_data.json'
+    # print('📝 Creando archivo JSON temporal...')
+    # with open(temp_file_path, 'w', encoding='utf-8') as temp_file:
+    #     for doc in example_docs:
+    #         temp_file.write(json.dumps({k: serialize_value(v) for k, v in doc.items()}, ensure_ascii=False) + '\n')
+    # print('📝 Archivo JSON temporal creado:', temp_file_path)
 
-    # Crear esquema y tabla BigQuery
-    fields = list(set(f.lower() for f in fields))
-    schema = [bigquery.SchemaField(f, "STRING", mode="NULLABLE") for f in fields]
-    table_ref = bigquery_client.dataset(var_dataset_id).table(var_table_id)
-    bigquery_client.create_table(bigquery.Table(table_ref, schema=schema), exists_ok=True)
+    # # Crear esquema y tabla BigQuery
+    # fields = list(set(f.lower() for f in fields))
+    # schema = [bigquery.SchemaField(f, "STRING", mode="NULLABLE") for f in fields]
+    # table_ref = bigquery_client.dataset(var_dataset_id).table(var_table_id)
+    # bigquery_client.create_table(bigquery.Table(table_ref, schema=schema), exists_ok=True)
 
-    # -----------------------
-    # Cargar tabla temporal
-    # -----------------------
-    print('📝 Creando tabla temporal...')
-    temp_table_id = var_table_id + "_temp"
-    temp_table_ref = bigquery_client.dataset(var_dataset_id).table(temp_table_id)
-    bigquery_client.create_table(bigquery.Table(temp_table_ref, schema=schema), exists_ok=True)
+    # # -----------------------
+    # # Cargar tabla temporal
+    # # -----------------------
+    # print('📝 Creando tabla temporal...')
+    # temp_table_id = var_table_id + "_temp"
+    # temp_table_ref = bigquery_client.dataset(var_dataset_id).table(temp_table_id)
+    # bigquery_client.create_table(bigquery.Table(temp_table_ref, schema=schema), exists_ok=True)
 
-    job_config_temp = bigquery.LoadJobConfig(
-        schema=schema,
-        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
-        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-        autodetect=True,
-        max_bad_records=50
-    )
-    with open(temp_file_path, "rb") as source_file:
-        print('📝 Cargando tabla temporal...')
-        bigquery_client.load_table_from_file(source_file, temp_table_ref, job_config=job_config_temp).result()
+    # job_config_temp = bigquery.LoadJobConfig(
+    #     schema=schema,
+    #     source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    #     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    #     autodetect=True,
+    #     max_bad_records=50
+    # )
+    # with open(temp_file_path, "rb") as source_file:
+    #     print('📝 Cargando tabla temporal...')
+    #     bigquery_client.load_table_from_file(source_file, temp_table_ref, job_config=job_config_temp).result()
 
-    # -----------------------
-    # MERGE / DELETE si incremental
-    # -----------------------
-    if not full_export:
-        print('📝 Merge / Delete si incremental...')
-        merge_sql = f"""
-            MERGE `{var_dataset_id}.{var_table_id}` T
-            USING `{var_dataset_id}.{temp_table_id}` S
-            ON T.id = S.id
-            WHEN MATCHED THEN UPDATE SET {', '.join([f'T.{f} = S.{f}' for f in fields])}
-            WHEN NOT MATCHED THEN INSERT ({', '.join(fields)}) VALUES ({', '.join([f'S.{f}' for f in fields])})
-        """
-        bigquery_client.query(merge_sql).result()
+    # # -----------------------
+    # # MERGE / DELETE si incremental
+    # # -----------------------
+    # if not full_export:
+    #     print('📝 Merge / Delete si incremental...')
+    #     merge_sql = f"""
+    #         MERGE `{var_dataset_id}.{var_table_id}` T
+    #         USING `{var_dataset_id}.{temp_table_id}` S
+    #         ON T.id = S.id
+    #         WHEN MATCHED THEN UPDATE SET {', '.join([f'T.{f} = S.{f}' for f in fields])}
+    #         WHEN NOT MATCHED THEN INSERT ({', '.join(fields)}) VALUES ({', '.join([f'S.{f}' for f in fields])})
+    #     """
+    #     bigquery_client.query(merge_sql).result()
 
-        delete_sql = f"""
-            DELETE FROM `{var_dataset_id}.{var_table_id}`
-            WHERE id NOT IN (SELECT id FROM `{var_dataset_id}.{temp_table_id}`)
-        """
-        bigquery_client.query(delete_sql).result()
-        print(f"✅ Datos cargados en la tabla {var_table_id} en BigQuery")
+    #     delete_sql = f"""
+    #         DELETE FROM `{var_dataset_id}.{var_table_id}`
+    #         WHERE id NOT IN (SELECT id FROM `{var_dataset_id}.{temp_table_id}`)
+    #     """
+    #     bigquery_client.query(delete_sql).result()
+    #     print(f"✅ Datos cargados en la tabla {var_table_id} en BigQuery")
 
-    else:
-        print('📝 Full export: sobrescribe la tabla...')
-        # Full export: sobrescribe la tabla
-        job_config_full = bigquery.LoadJobConfig(
-            schema=schema,
-            source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
-            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-            autodetect=True,
-            max_bad_records=50
-        )
-        with open(temp_file_path, "rb") as source_file:
-            bigquery_client.load_table_from_file(source_file, table_ref, job_config=job_config_full).result()
-        print(f"✅ Datos cargados en la tabla {var_table_id} en BigQuery")
+    # else:
+    #     print('📝 Full export: sobrescribe la tabla...')
+    #     # Full export: sobrescribe la tabla
+    #     job_config_full = bigquery.LoadJobConfig(
+    #         schema=schema,
+    #         source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    #         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    #         autodetect=True,
+    #         max_bad_records=50
+    #     )
+    #     with open(temp_file_path, "rb") as source_file:
+    #         bigquery_client.load_table_from_file(source_file, table_ref, job_config=job_config_full).result()
+    #     print(f"✅ Datos cargados en la tabla {var_table_id} en BigQuery")
 
     duration = (datetime.now(timezone.utc) - start_time).total_seconds()
     return ({'message': f'{len(example_docs)} documentos cargados en {var_table_id}.', 'duration_seconds': round(duration, 2)}), 200
