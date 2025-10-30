b10aa6d367c5ba1c9ca36a1f7d1eed5730a2f964
ana.medina <ana.medina@segurosbolivar.com>
2025-10-30 16:09:41 -0500

    Agregar log de lote

--- main.py
+++ main.py
@@ -208,12 +208,16 @@ def process_collection(firestore_client, collection_name, sep='_', max_level=2, 
     # -----------------------
     with concurrent.futures.ThreadPoolExecutor() as executor:
         while True:
-            # Ordenar por __name__ garantiza paginación estable
-            query = collection_ref.order_by("__name__").limit(page_size)
+            query = collection_ref.order_by(updated_field).order_by("__name__").limit(page_size)
+            
             if last_doc:
-                query = query.start_after({"__name__": last_doc.id})
+                last_value = last_doc.to_dict().get(updated_field)
+                if last_value is not None:
+                    query = query.start_after({updated_field: last_value, "__name__": last_doc.id})
+                else:
+                    query = query.start_after({"__name__": last_doc.id})
 
-            docs = safe_stream(query)
+            docs = list(query.stream())
             if not docs:
                 break
 
