fd53db4b17920a413de9249d35174a08d38a0d47
ana.medina <ana.medina@segurosbolivar.com>
2025-11-12 17:31:42 -0500

    Modificar fechas

--- main.py
+++ main.py
@@ -254,14 +254,12 @@ def process_collection(firestore_client, collection_name, sep='_', max_level=2, 
     # -----------------------
     with concurrent.futures.ThreadPoolExecutor() as executor:
         while True:
-            query = collection_ref.order_by(updated_field).order_by("__name__").limit(page_size)
+            # Ordenar solo por el campo filtrado para evitar índice compuesto
+            query = collection_ref.order_by(updated_field).limit(page_size)
 
             if last_doc:
-                last_value = last_doc.to_dict().get(updated_field)
-                if last_value is not None:
-                    query = query.start_after({updated_field: last_value, "__name__": last_doc.id})
-                else:
-                    query = query.start_after({"__name__": last_doc.id})
+                # Paginar usando el snapshot para evitar cursores compuestos
+                query = query.start_after(last_doc)
 
             docs = list(query.stream())
             if not docs:
